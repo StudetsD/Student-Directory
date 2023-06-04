5dbd422e84bb0fbd5d53312aa021b6032bee5676
Shakal <itsmy6328@gmail.com>
2023-06-04 18:22:47 +0600

    Добавил путь до экрана расписания

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -3,4 +3,5 @@ import '../features/chose_group/chose_group.dart';
 final routes = {
   '/': (context) => ChoseGroupScreen(),
   '/menu': (context) => const MenuPage(""),
+  '/schedule': (context) => const ScheduleWebPage(),
 };
