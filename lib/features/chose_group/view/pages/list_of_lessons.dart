8aa0ae85a1587371bf0b027a0649c6997d9f2ab4
Shakal <itsmy6328@gmail.com>
2023-06-07 01:26:11 +0600

    заготовка под экран

--- lib/features/chose_group/view/pages/list_of_lessons.dart
+++ lib/features/chose_group/view/pages/list_of_lessons.dart
@@ -1,4 +1,6 @@
+import 'dart:ui' as ui;
 import 'package:flutter/material.dart';
+import 'package:applicate/core/app_export.dart';
 
 class ListOfLessons extends StatelessWidget {
 
@@ -6,11 +8,53 @@ class ListOfLessons extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Scaffold(
-        body: Text("Список предметов"),
-      ),
+    return Stack(
+      fit: StackFit.expand,
+      children: <Widget>[
+        Container(
+          height: double.infinity,
+          width: double.infinity,
+          color: ColorConstant.blueFon,
+        ),
+        BackdropFilter(
+          filter: ui.ImageFilter.blur(
+            sigmaX: 8.0,
+            sigmaY: 8.0,
+          ),
+          child: Container(
+            color: Colors.transparent,
+              child: Text("Список предметов")
+          ),
+        ),
+      ],
     );
-  }
+      /*SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.blue,
+        body: Container(
+          child: Container(
+              decoration: AppDecoration.outline,
+              child: Stack(
+                fit: StackFit.expand,
+                children: <Widget>[
+                  BackdropFilter(
+                    filter: ui.ImageFilter.blur(
+                      sigmaX: 8.0,
+                      sigmaY: 8.0,
+                    ),
+                    child: Container(
+                      color: Colors.transparent,
+                    ),
+                  ),
+                  Center(
+                      child: Text("Список предметов")
+                  ),
+                ],
+              )
 
+            )
+        ),
+      ),
+    );*/
+  }
 }
